4e8ddd26c5f95b20e724df6d4d3f9585edef4600
Francisco Argiro <franciscoargiro@gmail.com>
2017-06-16 07:51:36 -0300

    Navegación más fluida, el header se esconde y aparece logo al hacer scroll, mejore el responsibe con animation del logo y acomode el menu

--- js/miCodigoJs.js
+++ js/miCodigoJs.js
@@ -7,22 +7,23 @@ $(function(){
 
 
 
-
+	// Scroll del encabezado y footer
 	$(window).scroll(function(){ 
 
 		var pxScroll = $(window).scrollTop(); // obtengo el dato del scroll en px por defecto
 
 		if (pxScroll!=0){
 			$("#logoSocRural").hide(500);
-			$("#encabezado").addClass("encabezado2");
+			$("#encabezado").addClass("encabezado2"); // fixed
 			//$("#contenido").addClass("wrapper");
 			$("#footer1").hide(500);
 			$("#marcador").css('visibility','visible');
+			$("#miniLogoSR30").css('visibility','visible');
 
 
 		} else if (pxScroll==0) {
-			$("#logoSocRural").show(500);
-			$("#encabezado").removeClass("encabezado2");
+			//$("#logoSocRural").show(500);  Prefiero no mostrarlo 
+			$("#encabezado").removeClass("encabezado2"); // No Fixed al top
 			//$("#contenido").removeClass("wrapper");
 		};
 		
@@ -30,12 +31,27 @@ $(function(){
 
 		
 
-
+	// Muestro el footer cuando esta escondido al hacerle click al marcador
 	$("#marcador").click(function(){
 		$("#footer1").show(500);
 		$("#marcador").css('visibility','hidden');
 		
 	});
+
+
+
+	$("#miniLogoSR30").click(function(){
+		$("#encabezado").removeClass("encabezado2");
+		$("html, body").animate({ scrollTop: -1 }, "slow");
+		$("#logoSocRural").show(500);
+		$("#miniLogoSR30").css('visibility','hidden');
+	});
+
+
+
+
+
+
 
 
 
